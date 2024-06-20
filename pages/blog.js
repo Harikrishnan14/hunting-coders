8af26477b3f84b0eaec3e1b51d1b8576ca7e4063
Harikrishnan14 <harikrishnankhd09@gmail.com>
2024-06-20 22:18:15 +0530

    Blog and BlogPost pages

--- pages/blog.js
+++ pages/blog.js
@@ -1,9 +1,36 @@
 import React from 'react'
+import styles from '../styles/Blog.module.css'
+import Link from 'next/link'
 
 const Blog = () => {
     return (
-        <div>
-            Blog Home
+        <div className={styles.container}>
+
+            <main className={styles.main}>
+
+                <div className={styles.blogItem}>
+                    <Link href='/blogpost/learn-javascript'>
+                        <h3>NextJs From Scratch</h3>
+                    </Link>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut error ratione voluptatum vero quasi odio!</p>
+                </div>
+
+                <div className={styles.blogItem}>
+                    <Link href='/blogpost/learn-javascript'>
+                        <h3>NextJs From Scratch</h3>
+                    </Link>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut error ratione voluptatum vero quasi odio!</p>
+                </div>
+
+                <div className={styles.blogItem}>
+                    <Link href='/blogpost/learn-javascript'>
+                        <h3>NextJs From Scratch</h3>
+                    </Link>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut error ratione voluptatum vero quasi odio!</p>
+                </div>
+
+            </main>
+
         </div>
     )
 }
--- pages/blogpost/[slug].js
+++ pages/blogpost/[slug].js
@@ -1,5 +1,6 @@
 import { useRouter } from 'next/router'
 import React from 'react'
+import styles from '../../styles/BlogPost.module.css'
 
 const Slug = () => {
 
@@ -7,8 +8,15 @@ const Slug = () => {
     const { slug } = router.query
 
     return (
-        <div>
-            {slug}
+        <div className={styles.container}>
+            <main className={styles.main}>
+                <h1>Title of the page {slug}</h1>
+                <hr />
+                <div>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde, aliquid tempore nobis temporibus id voluptatibus cumque laborum dolorem officiis? Architecto hic cupiditate libero quas deleniti.
+                </div>
+
+            </main>
         </div>
     )
 }
